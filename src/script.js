c566065d26edb0e86b55cafa2fa67f398c341379
joeltorresssa <149121807+joeltorresssa@users.noreply.github.com>
2026-06-17 08:57:56 -0300

    ATUALIZAÇÕ 17-06
    TERCEIRA DOBRA FINALIZADA

--- src/script.js
+++ src/script.js
@@ -9,3 +9,17 @@ function atualizarResol() {
 }
 atualizarResol()
 window.addEventListener("resize", atualizarResol)
+
+// let container = document.getElementsByClassName("container")
+// let caixas = document.getElementsByClassName("cont")
+// let seta_esq = document.querySelector(".seta1")
+// let seta_dir = document.querySelector(".seta2")
+
+// seta_esq.onclick = ()=>{
+//     caixas[].style.transform = "translateX(50px)"
+//     console.log("clicado")
+// }
+
+// seta_dir.onclick = ()=>{
+//     console.log("clicado")
+// }
